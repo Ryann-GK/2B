4f3d034d38b79905037f72df7d26ca4e4f70f80e
Ryann-GK <santos.ryann@escola.pr.gov.br>
2025-09-04 11:27:16 -0300

    ...

---
+++ projeto2/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+     'Futebol',
+     'O que é BID',
+     'No futebol brasileiro, BID é a silga para Boletim Informativo Diario da Confederação Brasileira de Futebol (CBF), Funciona como  um cartorio do futebol nacional'
+)
+
+criaCartao (
+     'Roblox',
+     'O que é Anime Fighers',
+     'É um jogo o roblox que se baseia em um simulador de lutadores onde o objetivo era ficar masi forte e chegar no ultimo mundo'
+)
+
+criaCartao (
+     'Futebol',
+     'O que é Penalti',
+     'O Penalti e uma falta dentro da Grande Área'
+)
