7bbf1d2a19b8defb7410e3f298db30bca2e3773a
Ryann-GK <santos.ryann@escola.pr.gov.br>
2025-09-11 11:11:33 -0300

    .

--- projeto2/app.js
+++ projeto2/app.js
@@ -1,5 +1,8 @@
 function criaCartao (categoria, pergunta, resposta) {
 
-    console.log (categoria, pergunta, resposta)
+    let container =document.getElementById('container') 
+    let cartao = document.createElement ('article')
+    cartao.className = 'cartao' 
+
 
 }
