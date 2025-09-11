08920664a2def9dc2a9a5b60b4ee6332a2dc2af1
Ryann-GK <santos.ryann@escola.pr.gov.br>
2025-09-11 11:23:48 -0300

    .

--- projeto2/app.js
+++ projeto2/app.js
@@ -3,6 +3,19 @@ function criaCartao (categoria, pergunta, resposta) {
     let container =document.getElementById('container') 
     let cartao = document.createElement ('article')
     cartao.className = 'cartao' 
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+        <h3>Futebol</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>O que é Penalti</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>O Penalti e uma falta dentro da Grande Área</p>
+        </div>
+    </div>
+    `
+
+    container.appendChild(cartao)
 
 
 }
